589d7eb07b18963e2b947c953f26730c7396a5d2
Christopher Hein <chrishein@Christophers-MacBook-Pro-2.local>
2019-01-13 16:19:26 -0500

    added more comments to code. Renamed variable to make code easier to understand

--- assets/javascript/gracenote.js
+++ assets/javascript/gracenote.js
@@ -1,5 +1,4 @@
-
-// delcaring variables to use for the api call. Will wrap all of this in a function and pass arguments based on input provided
+// Delcaring variables to use for the api call. Will wrap all of this in a function and pass arguments based on input provided
 let apikey = "nkpdkxcq96bpkunwvw22bxdd";
 let date = moment().format("YYYY-MM-DD"); // setting the date for the current day
 let zip = "30306"; // place holder zip code for now. Have to supply either zip code or coordinates for this api
@@ -10,48 +9,41 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
+    
+    // Pulls info for the first 5 local theaters returned provided they have a showtime later than the current time.
     for (let g = 0; g < 5; g++) {
 
         let title = response[g].title;
         let firstShowTime = response[g].showtimes[0];
-        let showtimes = firstShowTime["dateTime"];
+        let showDate = firstShowTime["dateTime"];
         let theater = firstShowTime["theatre"]["name"];
         let description = response[g]["shortDescription"];
         let runTime = response[g]["runTime"];
         let ticketURL = firstShowTime["ticketURI"];
         let runTimeArray = [];
 
+        // Only display information if the movie is happening after the current time
+        if (moment(showDate) > moment() && (title !== undefined)) {
+    
+            runTimeArray.push(runTime.split(""));
+            console.log(response);
+            console.log("Title: " + title);
+            console.log("Show Date: " + moment(showDate).format("h:mma"));
+            console.log("Theater: " + theater);
+            console.log("Description: " + description);
 
-        if (moment(showtimes) > moment()) {
-            console.log("you got the math right or something")
-        }
+            // Provides a URL to purchase tickets (if one exists)
+            if (ticketURL !== undefined) {
+                console.log("Ticket URL " + ticketURL);
 
-        runTimeArray.push(runTime.split(""));
-
-        console.log(response);
-        console.log("title " + title);
-        console.log("showtimes " + moment(showtimes).format("hh:mm a"));
-        console.log("theater " + theater);
-        console.log("description " + description);
-        //console.log("Raw Runtime " + runTimeArray.push(runTime.split("")));
-        if (ticketURL !== undefined) {
-            console.log("Ticket URL " + ticketURL);
-        } else {
-            console.log("Sorry, no link available for this show");
-        }
-        console.log("Run Time Array " + runTimeArray);
-        console.log("Run Time: " + runTimeArray[0][2] + runTimeArray[0][3] + " hrs " + runTimeArray[0][5] + runTimeArray[0][6] + " mins")
-
-        $("body").prepend(`
-        <div>
-            
-            <p>Uh oh....something's not quite right. Try again later</p>
-            <img src='https://media2.giphy.com/media/xT9IgIc0lryrxvqVGM/giphy-downsized.gif'/>
-        </div>
-        `
-        )
+                // This message will display if the theater doesn't provide a URL to purchase tickets
+            } else {
+                console.log("Sorry, there's no link available to purchase tickets for this show. Please check with the theater for tickets");
+            }
+            console.log("Run Time: " + runTimeArray[0][3] + " hrs " + runTimeArray[0][5] + runTimeArray[0][6] + " mins")
+        } 
     }
-
+    // Error message should something go wrong with the api call. Prepends to the body for now until there is a div to place it in
 }).catch(function (event) {
     $("body").prepend(`
     <div class="errorMessage">
