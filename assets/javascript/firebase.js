0fb0e3c8fe41d97b1b80689fcd33fd720bd81d13
Christopher Hein <chrishein@ChristohersMBP2.attlocal.net>
2019-01-16 22:59:11 -0500

    firebase authenticates users and logs userID and sign in time in database

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,91 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC6FdN1eU0ibIw_DP-fhOABKH0OS06e-pg",
+    authDomain: "what-s-happenin.firebaseapp.com",
+    databaseURL: "https://what-s-happenin.firebaseio.com",
+    projectId: "what-s-happenin",
+    storageBucket: "",
+    messagingSenderId: "513910723970"
+};
+firebase.initializeApp(config);
+
+let database = firebase.database();
+
+// declaring sign in function
+function signIn(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function (data) {
+            let databaseObject = {
+                "User ID": data["user"]["uid"],
+                "Sign In Time": moment().format("x"),
+            }
+            database.ref().push(databaseObject);
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            $("#signIn").append(`<p>${errorMessage}</p>`)
+        });
+}
+// run sign in functino when clicking sign in button
+$("#sign-in").on("click", function (event) {
+    event.preventDefault();
+    const email = $("#signEmail").val().trim();
+    const password = $("#signPassword").val().trim();
+  signIn(email, password);
+})
+
+// run sign out function when clicking sign out button
+$("#sign-out").on("click", function (event) {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    })
+        .then(function (data) {
+            let signOutObject = {
+                "User ID": data["user"]["uid"],
+                "Sign Out Time": moment().format("x")
+            }
+            database.ref().push(signOutObject);
+        })
+        .catch(function (error) {
+            // An error happened.
+        })
+});
+
+// register user and automatically log them in
+$("#registerSubmit").on("click", function () {
+    const email = $("#registerEmail").val().trim();
+    const password = $("#registerPassword").val().trim();
+    const first_name = $("#first_name").val().trim()
+    const last_name = $("#last_name").val().trim()
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function (data) {
+            signIn(email, password);
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            $("#registrationForm").append(`<p>${errorMessage}</p>`)
+        })
+});
+
+// persistence state to log out user upon closing the window
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function () {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+    })
+    .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        //$("body").prepend(`<p>${errorMessage}</p>`)
+    });
