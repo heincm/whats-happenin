17d9f719b53e8b1769e9d980e20f0d16cb3d5a67
Christopher Hein <chrishein@Christophers-MacBook-Pro-2.local>
2019-01-18 16:44:21 -0500

    call getMovies function on button click, get location, pass arguments

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,6 +10,9 @@
       $("#googlePlaceholder").hide();
       $("#map").show();
       let timeTokill = $("#timeOptions option:selected").val();
+      let latPosition = localStorage.getItem("lat");
+      let longPosition = localStorage.getItem("lng");
+      getMovies(latPosition, longPosition, timeTokill);
     });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
